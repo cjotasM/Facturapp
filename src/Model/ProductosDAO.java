3bc7f5a1e0ded9dbfd6663d2fc63a4a3e339842d
cjotas <ansaa2828@gmail.com>
2024-05-29 12:36:36 -0500

    commit con cambios hechos por Alvaro

--- src/Model/ProductosDAO.java
+++ src/Model/ProductosDAO.java
@@ -1,13 +1,131 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
 
-/**
- *
- * @author mares
- */
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.SQLException;
+    import javax.swing.JComboBox;
+    import java.sql.ResultSet;
+    import java.util.ArrayList;
+    import java.util.List;
+
 public class ProductosDAO {
     
+    Connection con;
+    Conexion cn = new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+
+    
+    public boolean RegistrarProductos(Productos pro){
+        String sql = "INSERT INTO productos (codigo, nombre, proveedor, stock, precio) VALUES (?,?,?,?,?)";
+        try {
+                con = cn.getConnection();
+                ps = con.prepareStatement(sql);
+                ps.setString(1, pro.getCodigo());
+                ps.setString(2, pro.getNombre());
+                ps.setString(3, pro.getProveedor());
+                ps.setInt(4, pro.getStock());
+                ps.setDouble(5, pro.getPrecio());
+                ps.execute();
+                return true;
+                
+        }catch (SQLException e) {
+                System.out.println(e.toString());
+                return false;
+        }
+    }
+
+    
+
+    public void ConsultarProveedor(JComboBox proveedor) {
+        String sql = "SELECT nombre FROM proveedores";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                proveedor.addItem(rs.getString("nombre"));
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+
+        }
+    }
+    
+    
+    
+    public List listarProductos() {
+        List<Productos> listapro = new ArrayList();
+        String sql = "SELECT * FROM productos";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                Productos pro = new Productos();
+                pro.setId(rs.getInt("id"));
+                pro.setCodigo(rs.getString("codigo"));
+                pro.setNombre(rs.getString("nombre"));
+                pro.setProveedor(rs.getString("proveedor"));
+                pro.setStock(rs.getInt("stock"));
+                pro.setPrecio(rs.getDouble("precio"));
+                listapro.add(pro);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return listapro;
+    }
+    
+    
+    
+    public boolean eliminarProductos(int id) {
+        String sql = "DELETE FROM productos WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
+    
+    
+    
+    public boolean modificarProductos(Productos pro) {
+        String sql = "UPDATE productos SET codigo = ?, nombre = ?, proveedor = ?, stock = ?, precio = ? WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pro.getCodigo());
+            ps.setString(2, pro.getNombre());
+            ps.setString(3, pro.getProveedor());
+            ps.setInt(4, pro.getStock());
+            ps.setDouble(5, pro.getPrecio());
+            ps.setInt(6, pro.getId());
+            ps.execute();
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
